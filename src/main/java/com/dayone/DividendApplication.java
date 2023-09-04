d7744026a2a80cb772fade11badd1662ec294735
joonlim6 <limjoon629@gmail.com>
2023-09-04 23:34:08 +0900

    API 인터페이스 + 엔티티 구현

--- src/main/java/com/dayone/DividendApplication.java
+++ src/main/java/com/dayone/DividendApplication.java
@@ -1,4 +1,4 @@
-package com.example.dividend;
+package com.dayone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dayone/DividendApplicationTests.java
+++ src/test/java/com/dayone/DividendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.dividend;
+package com.dayone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
