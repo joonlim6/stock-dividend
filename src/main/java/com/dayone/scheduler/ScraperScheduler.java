e282659f411ccf659d68e3fd896ad7cfa85d3828
joonlim6 <limjoon629@gmail.com>
2023-09-07 09:27:44 +0900

    로그 처리

--- src/main/java/com/dayone/scheduler/ScraperScheduler.java
+++ src/main/java/com/dayone/scheduler/ScraperScheduler.java
@@ -42,6 +42,7 @@ public class ScraperScheduler {
                     boolean exists = this.dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
                     if(!exists) {
                         this.dividendRepository.save(e);
+                        log.info("insert new dividend -> " + e.toString());
                     }
                 });
 
